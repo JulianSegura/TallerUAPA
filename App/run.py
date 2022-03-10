5f38293d57d117ad6739573cb956dc259fcb635b
Julian Segura <julianl.segura@gmail.com>
2022-03-10 00:41:33 -0400

    Cargado_Inicial

---
+++ App/run.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+import sqlite3
+
+#create controllers App
+app=Flask(__name__)
+
+
+
+
+
+
+#Application Entry Point
+if __name__ == "__main__":
+    app.run(debug=True)
+
