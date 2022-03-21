5858123a9cb62225695fba696ca340d1a1b165d7
Julian Segura <julianl.segura@gmail.com>
2022-03-20 21:48:48 -0400

    Register service orders ready

--- App/db.py
+++ App/db.py
@@ -1,3 +1,4 @@
+import datetime
 import sqlite3
 
 #connect database
@@ -12,26 +13,54 @@ def GetAllMarcas():
 
 def GetMarcasActivas():
     dbReader=sqlite3.connect(dbPath).cursor()
-    marcasActivas=dbReader.execute("select * from Marcas where activo=1").fetchall()
+    marcasActivas=dbReader.execute("select marcaid,marcanombre from Marcas where activo='true'").fetchall()
     dbReader.close()
-    return marcasActivas
+    return list(marcasActivas)
 
 def GetModelosPorMarca(marca):
     dbReader=sqlite3.connect(dbPath).cursor()
-    Modelos=dbReader.execute(f"select * from Modelos where MarcaId='{str(marca)}'").fetchall()
+    Modelos=dbReader.execute(f"select modeloid,modelonombre from Modelos where MarcaId={str(marca)} and activo='true'").fetchall()
     dbReader.close()
-    return Modelos
+    return list(Modelos)
 
-def ConsultaOSCliente(orden):
+def GetOSCliente(orden):
     dbReader=sqlite3.connect(dbPath).cursor()
-    Orden_Servicio=dbReader.execute(f"select * from ConsultaOSUsuario where OrdenId='{str(orden)}'").fetchone()
+    Orden_Servicio_db=dbReader.execute(f"select * from ConsultaOSUsuario where OrdenId='{str(orden)}'").fetchone()
     dbReader.close()
 
-    if Orden_Servicio:
-        return Orden_Servicio
+    if Orden_Servicio_db:
+        orden_servicio=list(Orden_Servicio_db)
+        for dato in orden_servicio:
+            if not dato: orden_servicio[orden_servicio.index(dato)]=0
+        return orden_servicio
+
+def GetAllOrdenesServicio():
+    dbReader=sqlite3.connect(dbPath).cursor()
+    Ordenes_Servicio=dbReader.execute("select * from ConsultaOS").fetchall()
+    dbReader.close()
+
+    if Ordenes_Servicio:
+        return list(Ordenes_Servicio)
+
+def CreateServiceOrder(OSrequest):   
+    insert_params=""
+    for param in OSrequest:
+        insert_params+=  f"'{param}',"
+    insert_params+=f"'{str(datetime.datetime.now())}','Para Revisar'"
+    insert_query="INSERT INTO OrdenesServicio (ClienteNombre,ClienteTelefono,MarcaId,ModeloId,Año,Chasis,FallaReportada,FechaRecepcion,Estado) VALUES ("+insert_params+");"
+    
+    conn=sqlite3.connect(dbPath,isolation_level=None)
+    dbCursor=conn.cursor()
+    with conn:
+        dbCursor.execute(insert_query)
+        conn.commit()
+    dbCursor.close()
+    conn.close()
+
+    created=True
+    if created:return True
 
 def GetUsuario(username):
-    
     dbReader=sqlite3.connect(dbPath).cursor()
     usuariodb=dbReader.execute(f"select * from consultausuariosactivos where usuario='{str(username)}'").fetchone()
     dbReader.close()
@@ -41,7 +70,7 @@ def GetUsuario(username):
 def Login(loginInfo):
     #ToDo: ver como hacer un hash de la contraseña
     usuariodb=GetUsuario(loginInfo[0])
-    if usuariodb != None and usuariodb[3]==loginInfo[1]:
-        return (usuariodb[0],usuariodb[1],usuariodb[4])
+    if usuariodb and usuariodb[3]==loginInfo[1]:
+        return (usuariodb[0],usuariodb[1],usuariodb[4]) #UserId, User Name, User Profile
     else:
         return None
--- App/run.py
+++ App/run.py
@@ -1,49 +1,118 @@
-from flask import Flask, redirect, render_template, request, url_for
+from datetime import timedelta
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
 import db
 
 
 #create controllers App
 app=Flask(__name__)
+app.secret_key="NuestraLlaveSecreta"
+app.permanent_session_lifetime=timedelta(minutes=10)
 
 #Index
 @app.route("/", methods=['GET','POST'])
 def Index():
-    OSQueryResult="emptyQuery"
     if request.method=='POST' and 'txtOrdenServicio' in request.form:
         NumeroOrden=request.form['txtOrdenServicio']
-        if NumeroOrden != "":
-            OSQueryResult=db.ConsultaOSCliente(NumeroOrden)
+        if NumeroOrden == "": return render_template("tallerindex.html",DatosConsulta="emptyQuery")
+        OSQueryResult=db.GetOSCliente(NumeroOrden)
+        return render_template("tallerindex.html",DatosConsulta=OSQueryResult)
+        
+    else:
+        #Jinja is not letting me clear the cache so i need to send the variable as empty
+        return render_template("tallerindex.html",DatosConsulta="emptyQuery")
 
-    return render_template("tallerindex.html",DatosConsulta=OSQueryResult)
 
+#Login Page
 @app.route("/login", methods=['GET','POST'])
 def Login():
-    LoginResults=""
+    session.clear()
     if request.method=='POST' and 'txtUsuario' and 'txtPassword' in request.form:
-        validInfo=request.form['txtUsuario']!="" and request.form['txtPassword']!=""
-        if not validInfo: 
-            LoginResults="Credenciales Invalidos"
-        else:
-            loginInfo=request.form['txtUsuario'],request.form['txtPassword']
-            LoginResults=db.Login(loginInfo)
-    
-    if LoginResults and LoginResults[2]=="Admin": return redirect(url_for('Admin'))
-    elif LoginResults and LoginResults[2]=="Mecanico": return redirect(url_for('Taller'))
-    else: return render_template("login.html",LoggedUser=LoginResults)
+        validInfo=request.form['txtUsuario'] != "" and request.form['txtPassword'] != ""
+        #If user or password is empty with return invalid credentials
+        if not validInfo: return render_template("login.html",Results="Credenciales Invalidos")
 
+        LoginResults=db.Login((request.form['txtUsuario'],request.form['txtPassword']))
+        
+        #if user cannot login will return nothing
+        if not LoginResults: return render_template("login.html",Results=None)
+
+        #load session dictionary and render appropiate template
+        session['userId']=LoginResults[0]
+        session['UserName']=LoginResults[1]
+        session['UserProfile']=LoginResults[2]
+        session.permanent=True
+
+        match session['UserProfile']:
+            case "Admin":
+                return redirect(url_for('Admin'))
+            case "Mecanico":
+                return redirect(url_for('Taller'))
+
+    #When method is not post, it will GET the page
+    else:
+        return render_template("login.html")
+
+#Admin Page
+@app.route("/admin", methods=['GET','POST'])
+def Admin():
+    if not session or session['UserProfile'] != "Admin":
+        return redirect(url_for('Login'))        
+    if request.method == 'GET': 
+        ordenesServicio=GetOrdenesServicio()
+        Marcas=db.GetMarcasActivas()
+        return render_template("admin.html",OrdenesServicio=ordenesServicio,marcas=Marcas)
+    else:
+        return RegisterOS()
+
+def GetOrdenesServicio():
+    ordenes=db.GetAllOrdenesServicio()
+    if not ordenes: return None
+    ordenesToReturn=[]
+    for orden in ordenes:
+        orden=list(orden)
+        #convert diasEnTaller to int
+        orden[2]=int(orden[2])
+        #if Reparacion have data, we return the reparacion data, else we return the Diagnostico data
+        if orden[9] == None:
+            orden.remove(orden[9])
+        else:
+            orden.remove(orden[8])
+        ordenesToReturn.append(orden)
+    return ordenesToReturn
+
+#Register Service Order
+def RegisterOS():
+    OSRequest=(request.form['txtNombreCliente'],request.form['txtTelefonoCliente'],request.form.get('cmbMarca'),request.form.get('cmbModelo'),
+    request.form['txtAno'],request.form['txtChasis'],request.form['txtReportedFailure'])
+    creation_result = db.CreateServiceOrder(OSRequest)
+    if creation_result:
+        flash("Orden De Servicio Creada Correctamente")
+        return redirect(url_for('Admin'))
+    else:
+        return "NO SE GUARDO"
+
+@app.route("/models/<marcaid>")
+def ModelsByBrand(marcaid):
+    models=db.GetModelosPorMarca(marcaid)
+    modelsToReturn=[]
+    for model in models:
+        modelobj={}
+        modelobj['modelId']=model[0]
+        modelobj['modelName']=model[1]
+        modelsToReturn.append(modelobj)
+    return jsonify({'models':modelsToReturn})
+
+#Taller Page
 @app.route("/taller")
 def Taller():
+    if not session or session['UserProfile']!="Mecanico":
+        return redirect(url_for('Login'))
     return "Pagina de personal de taller"
 
-@app.route("/admin")
-def Admin():
-    return render_template("admin.html")
-
-
+#Contact Page
 @app.route("/contacto")
 def Contacto():
     return "Aqui va la pagina para contactarnos"
-
 
 #Application Entry Point
 if __name__ == "__main__":
